a57bade7f1324243c04606e4411b46a4f048c88b
Jan Martin roelofs <jmartinr@home.nl>
2026-02-01 11:15:43 +0100

    cleanup

--- js/videojs.framebyframe.js
+++ js/videojs.framebyframe.js
@@ -5,66 +5,70 @@
 
 "use strict";
 
-const Button = videojs.getComponent('Button');
+class FrameByFrameButton extends videojs.getComponent('Button') {
+    constructor(player, options) {
+        super(player, options);
+        this.player = player;
+        this.frameTime = 1 / options.fps;
+        this.step_size = options.value;
+    }
 
-class FrameByFrameButton extends Button {
-  constructor(player, options) {
-    super(player, options);
-    this.player = player;
-    this.frameTime = 1/options.fps;
-    this.step_size = options.value;
-  }
-
-  handleClick() {
-    // Start by pausing the player
-    this.player.pause();
-    // Calculate movement distance
-    const dist = this.frameTime * this.step_size;
-    this.player.currentTime(this.player.currentTime() + dist);
-  }
+    handleClick() {
+        if (!this.player.paused()) {
+            this.player.pause();
+        }
+        this.player.currentTime(this.player.currentTime() + this.frameTime * this.step_size);
+    }
 }
 
 function framebyframe(options) {
     const player = this,
-          fps = options.fps ?? 30000/1001;
+        fps = options.fps ?? 30000 / 1001;
 
-    player.ready(function() {
-        options.steps.forEach(function(opt) {
+    player.ready(function () {
+        options.steps.forEach(function (opt) {
             const b = player.controlBar.addChild(
-                new FrameByFrameButton(player, {
-                    el: videojs.dom.createEl(
-                        'button',
-                        {
-                            className: 'vjs-res-button vjs-control',
-                            innerHTML: '<div class="vjs-control-content"><span class="vjs-fbf">' + opt.text + '</span></div>'
-                        },
-                        {
-                            role: 'button'
-                        }
-                    ),
-                    value: opt.step,
-                    fps: fps,
-                }),
-            {}, opt.index);
+                new FrameByFrameButton(
+                    player,
+                    {
+                        el: videojs.dom.createEl(
+                            'button',
+                            {
+                                className: 'vjs-res-button vjs-control',
+                                innerHTML: '<div class="vjs-control-content"><span class="vjs-fbf">' + opt.text + '</span></div>'
+                            },
+                            {
+                                role: 'button'
+                            }
+                        ),
+                        value: opt.step,
+                        fps: fps,
+                    }),
+                {}, opt.index);
             player.controlBar.el().insertBefore(b.el(), player.controlBar.fullscreenToggle.el());
         });
 
         // Add mouse wheel support
-        if(options.wheel){
-            player.el().addEventListener(
-                "wheel",
-                (event) => {
-                    const delta = event.deltaY;
-                    if (delta === 0) return;
-
-                    event.preventDefault();
-                    // Start by pausing the player
-                    player.pause();
-                    player.currentTime(player.currentTime() + Math.sign(delta) * options.wheel.step / fps);
-                },
-                { passive: false }
-            );
+        if (!options.wheel) {
+            return;
         }
+        player.el().addEventListener(
+            "wheel",
+            (event) => {
+                const delta = event.deltaY;
+                if (delta === 0) {
+                    return;
+                }
+
+                event.preventDefault();
+                if (!player.paused()) {
+                    player.pause();
+                }
+                player.currentTime(player.currentTime() + Math.sign(delta) * options.wheel.step / fps);
+            },
+            { passive: false }
+        );
+
     });
 }
 
